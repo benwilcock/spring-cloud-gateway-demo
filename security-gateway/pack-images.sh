f28edcc91ba5dc77e978afa0cba768970650910c
benwilcock <benwilcock@gmail.com>
2019-07-08 13:28:09 +0100

    renamed some components to start to differentiate them from others in the series

--- security-gateway/pack-images.sh
+++ security-gateway/pack-images.sh
@@ -4,8 +4,8 @@ echo "Performing a clean Maven build"
 ./mvnw clean package -DskipTests=true
 
 echo "Packing the Service"
-cd service
-pack build scg-demo-service --env "BP_JAVA_VERSION=8.*"
+cd secured-service
+pack build scg-demo-secured-service --env "BP_JAVA_VERSION=8.*"
 cd ..
 
 echo "Packing the Eureka Discovery Server"
@@ -14,6 +14,6 @@ pack build scg-demo-registry --env "BP_JAVA_VERSION=8.*"
 cd ..
 
 echo "Packing the Spring Cloud Gateway"
-cd gateway
-pack build scg-demo-gateway --env "BP_JAVA_VERSION=8.*"
+cd security-gateway
+pack build scg-demo-security-gateway --env "BP_JAVA_VERSION=8.*"
 cd ..
