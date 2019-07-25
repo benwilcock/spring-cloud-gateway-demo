0dcdbda00bbe7a5bf3f4ab3c67e0fef6108985bb
benwilcock <benwilcock@gmail.com>
2019-07-25 13:28:44 +0100

    Updated the architecture diagram and removed the unnecessary @EnableWebFluxSecurity annotation from the Resource Server

--- security-gateway/secured-service/src/main/java/com/scg/service/SecurityConfig.java
+++ security-gateway/secured-service/src/main/java/com/scg/service/SecurityConfig.java
@@ -1,11 +1,9 @@
 package com.scg.service;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
-@EnableWebFluxSecurity
 public class SecurityConfig {
 
   @Bean
